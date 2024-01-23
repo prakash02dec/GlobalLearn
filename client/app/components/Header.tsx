ae5258341ff7deaca73383a6b1ec8f0f015c6194
sudeepto147 <sudeepto11032001@gmail.com>
2024-01-23 18:18:28 +0530

    Navbar updated

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,5 +1,11 @@
 "use client";
+import Link from "next/link";
 import React, { FC, useState } from "react";
+import NavItems from "../utils/NavItems";
+import {ThemeSwitcher} from "../utils/ThemeSwitcher";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { HiOutlineUserCircle} from "react-icons/hi";
+
 
 type Props = {
   open: boolean;
@@ -7,7 +13,7 @@ type Props = {
   activeItem: number;
 };
 
-const Header: FC<Props> = (props) => {
+const Header: FC<Props> = ({activeItem, setOpen}) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
 
@@ -29,7 +35,41 @@ const Header: FC<Props> = (props) => {
             ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
             : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
         }`}
-      ></div>
+      >
+        <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+          <div className="w-full h-[80px] flex items-center justify-between p-3">
+            <div>
+              <Link
+                href={"/"}
+                className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+              >
+                GlobalLearn
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <NavItems 
+               activeItem={activeItem} 
+               isMobile={false}
+               />
+               <ThemeSwitcher/>
+               {/* Only for Mobile*/}
+               <div className="800px:hidden">
+                <HiOutlineMenuAlt3
+                 size={25}
+                 className="cursor-pointer dark:text-white text-black"
+                 onClick={() => setOpenSidebar(true)}
+                />
+               </div>
+               <HiOutlineUserCircle
+                size={25}
+                className="cursor-pointer dark:text-white text-black"
+                onClick={() => setOpen(true)}
+               />
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/app/utils/NavItems.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Link from "next/link"
+
+export const navItemsData = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Courses",
+    url: "/courses",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Policy",
+    url: "/policy",
+  },
+  {
+    name: "FAQ",
+    url: "/faq",
+  },
+];
+
+type Props = {
+  activeItem: number;
+  isMobile: boolean;
+}
+
+const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
+  return (
+    <>
+      <div className="hidden 800px:flex">
+        {navItemsData &&
+          navItemsData.map((i, index) => (
+            <Link href={`${i.url}`} key={index} passHref>
+              <span
+                className={`${
+                  activeItem === index
+                    ? "dark:text-[#37a39a] text-[crimson]"
+                    : "dark:text-white text-black"
+                } text-[18px] px-6 font-Poppins font-[400]`}
+              >
+                {i.name}
+              </span>
+            </Link>
+          ))}
+      </div>
+      {isMobile && (
+        <div className="800px:hidden mt-5">
+          <div className="w-full text-center py-6">
+            {navItemsData &&
+              navItemsData.map((i, index) => (
+                <Link href="/" passHref>
+                  <span
+                    className={`${
+                      activeItem === index
+                        ? "dark:text-[#37a39a] text-[crimson]"
+                        : "dark:text-white text-black"
+                    } block py-5 text-[18px] px-6 font-Poppins font-[400]`}
+                  >
+                    {i.name}
+                  </span>
+                </Link>
+              ))}
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default NavItems; 
