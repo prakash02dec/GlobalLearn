c06f9cc10a510f27f739709c871158bddf240133
Prathamesh P Pai <pratham_71@outlook.com>
2024-02-03 04:02:49 +0530

    Added course models and course controllers

--- server/app.ts
+++ server/app.ts
@@ -9,35 +9,36 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import errorMiddleware from './middleware/error';
 import userRouter from './routes/user.route';
+import courseRouter from './routes/course.route';
 
 // MIDLEWARES
 
 // body parser
-app.use(express.json({limit : "50mb"}));
+app.use(express.json({ limit: "50mb" }));
 // cookie parser to parse cookies coming from the fontend
 app.use(cookieParser());
 // cors = cross origin resource sharing
 // allows us to make secure requests from the our frontend to the backend server i.e inshort only allowed origins can make requests to the backend server
 app.use(cors({
-    origin: process.env.ORIGIN , // allow only this origin to make requests to the backend server
+    origin: process.env.ORIGIN, // allow only this origin to make requests to the backend server
 }))
 
 
 
 // routes
-app.use("/api/v1" , userRouter);
-
+app.use("/api/v1", userRouter);
+app.use("/api/v1", courseRouter);
 
 // Testing API
-app.get("/test" , (req : Request , res : Response , next : NextFunction )=> {
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({
-        success : true,
-        message : "API is working"
+        success: true,
+        message: "API is working"
     })
 });
 
 //  unknown route
-app.all("*" , (req : Request , res : Response , next : NextFunction )=> {
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
     const err = new Error(`Route ${req.originalUrl} not found`) as any;
     err.statusCode = 404;
     next(err);
---
+++ server/services/course.service.ts
@@ -0,0 +1,11 @@
+import { Response } from "express";
+import CourseModel from "../models/course.model";
+import { catchAsyncError } from "../middleware/catchAsyncError";
+
+export const createCourse = catchAsyncError(async (data: any, res: Response) => {
+  const course = await CourseModel.create(data);
+  res.status(201).json({
+    success: true,
+    course
+  });
+})
